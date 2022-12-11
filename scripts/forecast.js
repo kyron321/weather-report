53cb58154e5ce237bcfb8be4f81c5900c73415ff
Kyron <kds.060902@gmail.com>
2022-12-11 21:44:10 +0000

    created the basis of the getWeather function

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = "XAow6360EoREpnDORqZVn86yXsyHAJ21";
 
+//get weather information for city
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+//get city info
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
@@ -11,5 +23,10 @@ const getCity = async (city) => {
 };
 
 getCity("telford")
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then(data=>{
+    console.log(data)
+  })
+  .catch((err) => console.log(err));
