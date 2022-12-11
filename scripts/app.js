9f178dfb14b76908b7634c39d3abd96dba84cded
Kyron <kds.060902@gmail.com>
2022-12-11 22:32:35 +0000

    completed weather report

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,42 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+//updates data to the DOM
+const updateUI = (data) => {
+  const { cityDets, weather } = data;
+
+  details.innerHTML = `
+  <h5 class="my-3">${cityDets.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  </div>`;
+
+  //update night and day icon images
+
+  const iconScr = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconScr);
+
+  let timeScr = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeScr);
+
+  //remove d-none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
 
   return {
-    cityDets: cityDets,
-    weather: weather,
+    cityDets,
+    weather,
   };
 };
 
@@ -19,6 +49,6 @@ cityForm.addEventListener("submit", (e) => {
 
   //update with new city data
   updateCity(city)
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 });
